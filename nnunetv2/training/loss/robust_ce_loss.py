8cf35b299003a314d279c56f123e00a502b50ba0
Priyana Aragula <149327097+Pri0805@users.noreply.github.com>
2024-05-15 08:45:59 -0400

    Update robust_ce_loss.py with tverskeyloss

--- nnunetv2/training/loss/robust_ce_loss.py
+++ nnunetv2/training/loss/robust_ce_loss.py
@@ -30,3 +30,30 @@ class TopKLoss(RobustCrossEntropyLoss):
         num_voxels = np.prod(res.shape, dtype=np.int64)
         res, _ = torch.topk(res.view((-1, )), int(num_voxels * self.k / 100), sorted=False)
         return res.mean()
+
+#### EDITS MADE FOR TVERKSEY LOSS ####
+
+class TverskyLoss(RobustCrossEntropyLoss):
+    def __init__(self, alpha=0.3, beta=0.7, smooth=1e-6, **kwargs):
+        super(TverskyLoss, self).__init__(**kwargs)
+        self.alpha = alpha
+        self.beta = beta
+        self.smooth = smooth
+
+    def forward(self, net_output, target):
+        if target.ndim == net_output.ndim:
+            assert target.shape[1] == 1
+            target = target[:, 0]
+        
+        target_one_hot = torch.zeros_like(net_output)
+        target_one_hot.scatter_(1, target.long(), 1)
+
+        net_output = net_output.contiguous().view(-1)
+        target_one_hot = target_one_hot.contiguous().view(-1)
+
+        true_pos = torch.sum(target_one_hot * net_output)
+        false_neg = torch.sum(target_one_hot * (1 - net_output))
+        false_pos = torch.sum((1 - target_one_hot) * net_output)
+
+        tversky_index = (true_pos + self.smooth) / (true_pos + self.alpha * false_neg + self.beta * false_pos + self.smooth)
+        return 1 - tversky_index
