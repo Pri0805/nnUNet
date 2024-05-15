fc51f194b77d0493bfae4669e7b305c77c7d84db
Priyana Aragula <149327097+Pri0805@users.noreply.github.com>
2024-05-15 08:51:04 -0400

    Update compound_losses.py with tverskey loss

--- nnunetv2/training/loss/compound_losses.py
+++ nnunetv2/training/loss/compound_losses.py
@@ -1,6 +1,6 @@
 import torch
 from nnunetv2.training.loss.dice import SoftDiceLoss, MemoryEfficientSoftDiceLoss
-from nnunetv2.training.loss.robust_ce_loss import RobustCrossEntropyLoss, TopKLoss
+from nnunetv2.training.loss.robust_ce_loss import RobustCrossEntropyLoss, TopKLoss, TverskyLoss
 from nnunetv2.utilities.helpers import softmax_helper_dim1
 from torch import nn
 
@@ -148,3 +148,54 @@ class DC_and_topk_loss(nn.Module):
 
         result = self.weight_ce * ce_loss + self.weight_dice * dc_loss
         return result
+
+
+###### EDITS MADE FOR TVERKSEY LOSS, also added import TverskeyLoss statement from robost_ce_loss.py ######
+
+class CE_and_Tversky_loss(nn.Module):
+    def __init__(self, tversky_kwargs, ce_kwargs, weight_ce=1, weight_tversky=1, ignore_label=None):
+        """
+        Weights for CE and Tversky do not need to sum to one. You can set whatever you want.
+        :param tversky_kwargs:
+        :param ce_kwargs:
+        :param weight_ce:
+        :param weight_tversky:
+        :param ignore_label:
+        """
+        super(CE_and_Tversky_loss, self).__init__()
+        if ignore_label is not None:
+            ce_kwargs['ignore_index'] = ignore_label
+
+        self.weight_tversky = weight_tversky
+        self.weight_ce = weight_ce
+        self.ignore_label = ignore_label
+
+        self.ce = RobustCrossEntropyLoss(**ce_kwargs)
+        self.tversky = TverskyLoss(**tversky_kwargs)
+
+    def forward(self, net_output: torch.Tensor, target: torch.Tensor):
+        """
+        target must be b, c, x, y(, z) with c=1
+        :param net_output:
+        :param target:
+        :return:
+        """
+        if self.ignore_label is not None:
+            assert target.shape[1] == 1, 'ignore label is not implemented for one hot encoded target variables ' \
+                                         '(CE_and_Tversky_loss)'
+            mask = target != self.ignore_label
+            target_tversky = torch.where(mask, target, 0)
+            num_fg = mask.sum()
+        else:
+            target_tversky = target
+            mask = None
+
+        tversky_loss = self.tversky(net_output, target_tversky) \
+            if self.weight_tversky != 0 else 0
+        ce_loss = self.ce(net_output, target[:, 0]) \
+            if self.weight_ce != 0 and (self.ignore_label is None or num_fg > 0) else 0
+
+        result = self.weight_ce * ce_loss + self.weight_tversky * tversky_loss
+        return result
+
+
